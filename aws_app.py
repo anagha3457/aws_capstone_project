1d21ca38db0c06a651092c46434f191f76700df6
anagha3457 <anaghapingale2@gmail.com>
2026-02-06 19:24:12 +0530

    Update aws_app.py

--- aws_app.py
+++ aws_app.py
@@ -470,9 +470,4 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
-# RUN APP
-
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000, debug=True)
 
